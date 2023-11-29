4b4387378e975fe5899373e134d9f9382ceb4619
akbarshox-dev <akbarshoxdev@gmail.com>
2023-11-29 15:36:34 +0500

    Product cart component created

---
+++ src/components/product-cart/product-cart.jsx
@@ -0,0 +1,13 @@
+import './product-cart.scss';
+
+export default function ProductCart ({ id, image, name, price }) {
+    return <div className='product'>
+        <img src={image} alt={name} className="product__image" />
+        <div className="product__text">
+            <h2 className='product__text-name'>{ name } </h2>
+            <p className='product__text-price'>{price}</p>
+        </div>
+        <button id-data={id} className='product__button'>Add</button>
+    </div>
+}
+
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,32 @@
 import React from "react"
+import ProductCart from "../components/product-cart/product-cart"
 
-export default function Home () {
+export default function Home() {
+    const products = [
+        {
+            id: 1,
+            name: "iPhone X",
+            image: "/iphone.jpg",
+            price: "170$"
+        },
+        {
+            id: 2,
+            name: "iPhone XS",
+            image: "/iphone.jpg",
+            price: "220$"
+        },
+        {
+            id: 3,
+            name: "iPhone XS Max",
+            image: "/iphone.jpg",
+            price: "250$"
+        },
+    ]
     return <>
-        <h1>Home Page</h1>
+        <div style={{ marginTop: "40px", display: "flex", flexWrap: "wrap", gap: "50px"}} className="products-box  container">
+            { products.map((item, index) => (
+                <ProductCart key={index} id={item.id} name={item.name} price={item.price} image={item.image} />
+            ))}
+        </div >
     </>
 }
